12fb7115fc46a8e929dd7884cea640bb344fc008
Somya Goel <somya.goel@nextuple.com>
2022-04-28 06:01:51 +0000

    Merged in changes-related-to-config (pull request #6)
    changes
    
    * changes
    
    
    Approved-by: Raghavendra Bhat K

--- core-near-cache-spring/src/main/java/com/nextuple/core/spring/config/MultipleCacheManagerConfig.java
+++ core-near-cache-spring/src/main/java/com/nextuple/core/spring/config/MultipleCacheManagerConfig.java
@@ -4,6 +4,7 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -14,21 +15,22 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
+@ConditionalOnProperty(value = "near-cache.enabled", havingValue = "true")
 public class MultipleCacheManagerConfig {
 
-  @Value("${near-cache.duration}")
+  @Value("${near-cache.duration:60}")
   private Long duration;
 
-  @Value("${near-cache.timeUnit}")
+  @Value("${near-cache.timeUnit:SECONDS}")
   private TimeUnit timeUnit;
 
-  @Value("${near-cache.maxSize}")
+  @Value("${near-cache.maxSize:100}")
   private Long maxSize;
 
-  @Value("${remote-cache.ttlInHours}")
+  @Value("${remote-cache.ttlInHours:4}")
   private Long ttlInHours;
 
-  @Value("${remote-cache.cachename}")
+  @Value("${remote-cache.cachename:bag_list_associated_with_last_four_digits}")
   private String redisCacheName;
 
   @Bean
