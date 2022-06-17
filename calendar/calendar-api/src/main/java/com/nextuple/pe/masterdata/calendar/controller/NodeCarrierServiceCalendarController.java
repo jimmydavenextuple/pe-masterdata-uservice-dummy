8b8ec8e331e7e6b569680f0cd44c2e158321bcb4
arihant263 <arihant.jain@nextuple.com>
2022-06-17 14:44:25 +0530

    calendar module

--- calendar/calendar-api/src/main/java/com/nextuple/pe/masterdata/calendar/controller/NodeCarrierServiceCalendarController.java
+++ calendar/calendar-api/src/main/java/com/nextuple/pe/masterdata/calendar/controller/NodeCarrierServiceCalendarController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/nodeCarrierServiceCalendar")
+@RequestMapping("/node-carrier-service-calendar")
 @RequiredArgsConstructor
 public class NodeCarrierServiceCalendarController {
 
