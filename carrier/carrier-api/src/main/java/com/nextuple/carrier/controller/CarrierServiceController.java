132f5822ab52b585fe4472b000e7564506f2f68d
somya <somya.goel@nextuple.com>
2022-06-17 14:14:21 +0530

    review changes

--- carrier/carrier-api/src/main/java/com/nextuple/carrier/controller/CarrierServiceController.java
+++ carrier/carrier-api/src/main/java/com/nextuple/carrier/controller/CarrierServiceController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/carrierService")
+@RequestMapping("/carrier-service")
 @RequiredArgsConstructor
 public class CarrierServiceController {
 
