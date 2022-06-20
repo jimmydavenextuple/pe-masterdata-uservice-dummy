0e2d725f32a89e04e92b7df0b4eb63130a0302d9
Priyanshu Joshi <priyanshu.joshi@nextuple.com>
2022-06-20 17:04:09 +0530

    Refactored Postal Code Timezone Near Cache Implementation

--- postal-code-timezone/postal-code-timezone-near-cache/src/main/java/com/nextuple/postal/code/timezone/cache/domain/PostalCodeTimezoneCacheValue.java
+++ postal-code-timezone/postal-code-timezone-near-cache/src/main/java/com/nextuple/postal/code/timezone/cache/domain/PostalCodeTimezoneCacheValue.java
@@ -1,6 +1,7 @@
 package com.nextuple.postal.code.timezone.cache.domain;
 
 import com.nextuple.core.cache.domain.CacheValue;
+import com.nextuple.postal.code.timezone.api.domain.dto.PostalCodeTimezoneDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- postal-code-timezone/postal-code-timezone-near-cache/src/main/java/com/nextuple/postal/code/timezone/cache/service/PostalCodeTimezoneFeignService.java
+++ postal-code-timezone/postal-code-timezone-near-cache/src/main/java/com/nextuple/postal/code/timezone/cache/service/PostalCodeTimezoneFeignService.java
@@ -2,7 +2,7 @@ package com.nextuple.postal.code.timezone.cache.service;
 
 import com.nextuple.common.response.BaseResponse;
 import com.nextuple.core.cache.service.GenericFeignService;
-import com.nextuple.postal.code.timezone.cache.domain.PostalCodeTimezoneDto;
+import com.nextuple.postal.code.timezone.api.domain.dto.PostalCodeTimezoneDto;
 
 public interface PostalCodeTimezoneFeignService
     extends GenericFeignService<String, BaseResponse<PostalCodeTimezoneDto>> {}
