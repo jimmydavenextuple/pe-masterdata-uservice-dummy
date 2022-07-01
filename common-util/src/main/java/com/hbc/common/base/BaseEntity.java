3a5eec96de62510b6b66eb038396a2754a7b7d3b
arihant-jain-vendor_bay <arihant.jain-vendor@hbc.com>
2022-07-01 19:15:38 +0530

    changes

--- common-util/src/main/java/com/hbc/common/base/BaseEntity.java
+++ common-util/src/main/java/com/hbc/common/base/BaseEntity.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
@@ -22,5 +21,5 @@ public abstract class BaseEntity {
   @Column(updatable = false)
   private Date createdDate;
 
-  @LastModifiedDate private Date lastModifiedDate;
+  private Date lastModifiedDate = new Date();
 }
