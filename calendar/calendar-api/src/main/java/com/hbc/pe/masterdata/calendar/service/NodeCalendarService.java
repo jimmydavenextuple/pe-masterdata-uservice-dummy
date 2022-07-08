7694d85298d935181943cf02607b2bec2df17594
somya goel <somya-goe-vendor_bay>
2022-07-07 23:33:54 -0400

    Added validation

--- calendar/calendar-api/src/main/java/com/hbc/pe/masterdata/calendar/service/NodeCalendarService.java
+++ calendar/calendar-api/src/main/java/com/hbc/pe/masterdata/calendar/service/NodeCalendarService.java
@@ -2,6 +2,7 @@ package com.hbc.pe.masterdata.calendar.service;
 
 import com.hbc.common.exception.CommonServiceException;
 import com.hbc.common.response.error.ErrorType;
+import com.hbc.common.response.error.FieldError;
 import com.hbc.pe.masterdata.calendar.domain.CalendarDomain;
 import com.hbc.pe.masterdata.calendar.domain.NodeCalendarDomain;
 import com.hbc.pe.masterdata.calendar.domain.entity.CalendarEntity;
@@ -11,23 +12,33 @@ import com.hbc.pe.masterdata.calendar.domain.mapper.CalendarMapper;
 import com.hbc.pe.masterdata.calendar.domain.outbound.CalendarResponse;
 import com.hbc.pe.masterdata.calendar.domain.outbound.NodeCalendarResponse;
 import com.hbc.pe.masterdata.calendar.exception.CalendarDomainException;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class NodeCalendarService {
 
+  private static final Logger logger = LoggerFactory.getLogger(NodeCalendarService.class);
   private static final CalendarMapper INSTANCE = Mappers.getMapper(CalendarMapper.class);
   private final NodeCalendarDomain nodeCalendarDomain;
-
+  private static final String ORG_ID = "orgId";
+  private static final String CALENDAR_ID = "calendarId";
+  private static final String NODE_EXCEPTION_MESSAGE = "Node not found with given details";
   private final CalendarDomain calendarDomain;
 
   /** Creates a new Node Calendar */
   public NodeCalendarResponse processCreateNodeCalendar(NodeCalendarRequest nodeCalendarRequest)
-      throws CalendarDomainException {
+      throws CalendarDomainException, CommonServiceException {
     validateCalendarId(nodeCalendarRequest.getCalendarId(), nodeCalendarRequest.getOrgId());
 
     NodeCalendarEntity nodeCalendarEntity =
@@ -48,9 +59,13 @@ public class NodeCalendarService {
           throws CalendarDomainException, CommonServiceException {
      CalendarEntity calendarEntity = calendarDomain.getCalendar(orgId, calendarId);
 
-    if(){
-      throw new CalendarDomainException(
-              "Cannot create a node calendar as the global calendar does not exists for the given calendarId and OrdId", null, calendarId, orgId, null, null);
+    if(calendarEntity == null){
+      logger.info(NODE_EXCEPTION_MESSAGE);
+      Map<String, FieldError> errorMap = new HashMap<>();
+      errorMap.put(ORG_ID, FieldError.builder().rejectedValue(orgId).build());
+      errorMap.put(CALENDAR_ID, FieldError.builder().rejectedValue(calendarId).build());
+      throw new CommonServiceException(
+              NODE_EXCEPTION_MESSAGE, HttpStatus.NOT_FOUND, 0x1771, errorMap);
     }
   }
 }
