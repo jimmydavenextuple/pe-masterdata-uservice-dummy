f73c5877a5d585cbccd4d90509b707fed61f37d4
Sridhar S <sridhar.s@nextuple.com>
2022-07-19 09:37:37 +0530

    Increased code coverage.

--- nfs-near-cache/core-near-cache-spring/src/test/java/com/hbc/core/spring/config/MultipleCacheManagerConfigTest.java
+++ nfs-near-cache/core-near-cache-spring/src/test/java/com/hbc/core/spring/config/MultipleCacheManagerConfigTest.java
@@ -1,13 +1,11 @@
 package com.hbc.core.spring.config;
 
-import static com.hbc.core.spring.config.CacheProperties.CACHE_PROPERTY_VALUE;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.hbc.core.spring.util.TestUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,13 +16,13 @@ import org.springframework.cache.CacheManager;
 @ExtendWith(MockitoExtension.class)
 class MultipleCacheManagerConfigTest {
 
-  @InjectMocks MultipleCacheManagerConfig multipleCacheManagerConfig;
-
+  @InjectMocks private MultipleCacheManagerConfig multipleCacheManagerConfig;
   @Mock private CacheProperties cacheProperties;
+  @InjectMocks private TestUtil testUtil;
 
   @Test
   void caffeineCacheManagerTest() {
-    when(cacheProperties.getCacheMap()).thenReturn(getCacheMap());
+    when(cacheProperties.getCacheMap()).thenReturn(testUtil.getCacheMap());
     CacheManager cacheManager = multipleCacheManagerConfig.caffeineCacheManager();
     assertNotNull(cacheManager);
     verify(cacheProperties, times(0)).setCacheDefaults();
@@ -33,22 +31,9 @@ class MultipleCacheManagerConfigTest {
   @Test
   void caffeineCacheManagerDefaultCachePropertiesTest() {
     when(cacheProperties.getCacheMap()).thenReturn(null);
+    when(cacheProperties.setCacheDefaults()).thenReturn(testUtil.getCacheMap());
     CacheManager cacheManager = multipleCacheManagerConfig.caffeineCacheManager();
     assertNotNull(cacheManager);
     verify(cacheProperties, times(1)).setCacheDefaults();
-  }
-
-  private Map<String, String> getCacheMap() {
-    Map<String, String> cacheMap = new HashMap<>();
-    cacheMap.put("node", CACHE_PROPERTY_VALUE);
-    cacheMap.put("carrier", CACHE_PROPERTY_VALUE);
-    cacheMap.put("node_carrier", CACHE_PROPERTY_VALUE);
-    cacheMap.put("node_calendar", CACHE_PROPERTY_VALUE);
-    cacheMap.put("carrier_calendar", CACHE_PROPERTY_VALUE);
-    cacheMap.put("node_carrier_calendar", CACHE_PROPERTY_VALUE);
-    cacheMap.put("postal code timezone", CACHE_PROPERTY_VALUE);
-    cacheMap.put("sourcingRule", CACHE_PROPERTY_VALUE);
-    cacheMap.put("weightage configuration", CACHE_PROPERTY_VALUE);
-    return cacheMap;
   }
 }
