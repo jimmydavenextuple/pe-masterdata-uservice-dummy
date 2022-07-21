4f0a6726d88affea8e17412fe56ae55040498a87
puneet-chopra-vendor_bay <somya-goe-vendor_bay>
2022-07-21 10:15:20 -0400

    Added validation for NodeUpdationRequest

--- node/node-api-domain/src/main/java/com/hbc/node/domain/inbound/NodeUpdationRequest.java
+++ node/node-api-domain/src/main/java/com/hbc/node/domain/inbound/NodeUpdationRequest.java
@@ -1,10 +1,13 @@
 package com.hbc.node.domain.inbound;
 
 import java.io.Serializable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
@@ -12,31 +15,54 @@ import lombok.NoArgsConstructor;
 @Builder
 public class NodeUpdationRequest implements Serializable {
 
+  @NotBlank(message = "street can't be blank")
+  @Length(max = 50)
   private String street;
 
+  @NotBlank(message = "city can't be blank")
+  @Length(max = 50)
   private String city;
 
+  @NotBlank(message = "timezone can't be blank")
+  @Length(max = 50)
   private String timezone;
 
+  @NotNull(message = "shipToHome can't be null")
   private Boolean shipToHome;
 
+  @NotNull(message = "sdndEligible can't be null")
   private Boolean sdndEligible;
 
+  @NotNull(message = "bopisEligible can't be null")
   private Boolean bopisEligible;
 
+  @NotNull(message = "expressEligible can't be null")
   private Boolean expressEligible;
 
+  @NotBlank(message = "nodeType can't be blank")
+  @Length(max = 50)
   private String nodeType;
 
+  @NotBlank(message = "province can't be blank")
+  @Length(max = 50)
   private String province;
 
+  @NotBlank(message = "postalCode can't be blank")
+  @Length(max = 50)
   private String postalCode;
 
+  @NotBlank(message = "country can't be blank")
+  @Length(max = 50)
   private String country;
 
+  @NotBlank(message = "latitude can't be blank")
+  @Length(max = 50)
   private String latitude;
 
+  @NotBlank(message = "longitude can't be blank")
+  @Length(max = 50)
   private String longitude;
 
+  @NotNull(message = "isActive can't be null")
   private Boolean isActive;
 }
