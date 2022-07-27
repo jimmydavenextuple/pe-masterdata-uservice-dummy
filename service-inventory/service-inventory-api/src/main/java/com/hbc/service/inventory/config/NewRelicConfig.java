7e17e33fa1a2fafe12892eef752053f042d9790f
mansisharmanextuple <mansi.sharma@nextuple.com>
2022-07-27 13:17:46 +0530

    NewRelic config profile set to non-default env Reason : Getting the env variables from the cluster

--- service-inventory/service-inventory-api/src/main/java/com/hbc/service/inventory/config/NewRelicConfig.java
+++ service-inventory/service-inventory-api/src/main/java/com/hbc/service/inventory/config/NewRelicConfig.java
@@ -5,7 +5,8 @@ import io.micrometer.NewRelicRegistryConfig;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.util.NamedThreadFactory;
 import io.micrometer.newrelic.NewRelicRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
@@ -14,47 +15,51 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.time.Duration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-@AutoConfigureBefore({CompositeMeterRegistryAutoConfiguration.class, SimpleMetricsExportAutoConfiguration.class})
+@AutoConfigureBefore({
+  CompositeMeterRegistryAutoConfiguration.class,
+  SimpleMetricsExportAutoConfiguration.class
+})
 @AutoConfigureAfter(MetricsAutoConfiguration.class)
 @ConditionalOnClass(NewRelicRegistry.class)
+@Profile("!default")
 public class NewRelicConfig {
 
-    @Bean
-    public NewRelicRegistry newRelicMeterRegistry(NewRelicRegistryConfig config) throws UnknownHostException {
-        NewRelicRegistry newRelicRegistry = NewRelicRegistry.builder(config).commonAttributes(new Attributes().put("host", InetAddress.getLocalHost().getHostName())).build();
-        newRelicRegistry.config().meterFilter(MeterFilter.ignoreTags("plz_ignore_me"));
-//        newRelicRegistry.config().meterFilter(MeterFilter.denyNameStartsWith("jvm.threads"));
-        newRelicRegistry.start(new NamedThreadFactory("newrelic.micrometer.registry"));
-        return newRelicRegistry;
-    }
+  @Bean
+  public NewRelicRegistry newRelicMeterRegistry(NewRelicRegistryConfig config)
+      throws UnknownHostException {
+    NewRelicRegistry newRelicRegistry =
+        NewRelicRegistry.builder(config)
+            .commonAttributes(
+                new Attributes().put("host", InetAddress.getLocalHost().getHostName()))
+            .build();
+    newRelicRegistry.config().meterFilter(MeterFilter.ignoreTags("plz_ignore_me"));
+    newRelicRegistry.start(new NamedThreadFactory("newrelic.micrometer.registry"));
+    return newRelicRegistry;
+  }
 
-    @Bean
-    public NewRelicRegistryConfig customNewRelicConfig() {
-        return new NewRelicRegistryConfig() {
-            @Override
-            public String get(String key) {
-                if (key.contains("apiKey")) {
-                    return apiKey();
-                }
-                return null;
-            }
+  @Bean
+  public NewRelicRegistryConfig customNewRelicConfig() {
+    return new NewRelicRegistryConfig() {
+      @Override
+      public String get(String key) {
+        if (key.contains("apiKey")) {
+          return apiKey();
+        }
+        return null;
+      }
 
-            @Override
-            public String apiKey() {
-                return System.getenv("NEW_RELIC_LICENSE_KEY");
-            }
+      @Override
+      public String apiKey() {
+        return System.getenv("NEW_RELIC_LICENSE_KEY");
+      }
 
-
-            @Override
-            public String serviceName() {
-                return System.getenv("NEW_RELIC_APP_NAME");
-            }
-        };
-    }
+      @Override
+      public String serviceName() {
+        return System.getenv("NEW_RELIC_APP_NAME");
+      }
+    };
+  }
 }
