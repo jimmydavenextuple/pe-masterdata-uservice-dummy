99f8d83b451ebee1c7a438143f368d7cbe1fa782
Raghavendra Bhat <raghavendra.bhat@nextuple.com>
2022-07-27 11:14:00 +0530

    NewRelic Config

---
+++ service-inventory/service-inventory-api/src/main/java/com/hbc/service/inventory/config/NewRelicConfig.java
@@ -0,0 +1,42 @@
+package com.hbc.service.inventory.config;
+
+import com.newrelic.telemetry.Attributes;
+import io.micrometer.NewRelicRegistryConfig;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+import io.micrometer.newrelic.NewRelicRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+@AutoConfigureBefore({
+        CompositeMeterRegistryAutoConfiguration.class,
+        SimpleMetricsExportAutoConfiguration.class
+})
+@AutoConfigureAfter(MetricsAutoConfiguration.class)
+@ConditionalOnClass(NewRelicRegistry.class)
+public class NewRelicConfig {
+    @Bean
+    public NewRelicRegistry newRelicMeterRegistry(NewRelicRegistryConfig config)
+            throws UnknownHostException {
+        NewRelicRegistry newRelicRegistry =
+                NewRelicRegistry.builder(config)
+                        .commonAttributes(
+                                new Attributes()
+                                        .put("host", InetAddress.getLocalHost().getHostName()))
+                        .build();
+        newRelicRegistry.config().meterFilter(MeterFilter.ignoreTags("plz_ignore_me"));
+        newRelicRegistry.config().meterFilter(MeterFilter.denyNameStartsWith("jvm.threads"));
+        newRelicRegistry.start(new NamedThreadFactory("newrelic.micrometer.registry"));
+        return newRelicRegistry;
+    }
+}
