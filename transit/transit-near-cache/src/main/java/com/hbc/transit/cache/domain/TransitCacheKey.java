4fdaff3898e924011f44b8d69b18392c6227cb16
rithik <rithik.sachdev-vendor@thebay.com>
2022-08-24 18:56:08 +0530

    Adding near cache

---
+++ transit/transit-near-cache/src/main/java/com/hbc/transit/cache/domain/TransitCacheKey.java
@@ -0,0 +1,17 @@
+package com.hbc.transit.cache.domain;
+
+import com.hbc.core.cache.domain.CacheKey;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class TransitCacheKey implements CacheKey {
+  public TransitCacheKey() {}
+
+  private String orgId;
+
+  private String destinationGeozone;
+}
