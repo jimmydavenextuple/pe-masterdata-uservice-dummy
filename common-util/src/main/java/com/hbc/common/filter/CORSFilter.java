c07a68a872438f6e9ef3136385aa2a66833975a3
Sridhar S <sridhar.s@nextuple.com>
2022-08-25 09:35:16 +0530

    Changed the order of precedence for CORS Filter.

--- common-util/src/main/java/com/hbc/common/filter/CORSFilter.java
+++ common-util/src/main/java/com/hbc/common/filter/CORSFilter.java
@@ -13,10 +13,13 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
 
   @Autowired Environment environment;
