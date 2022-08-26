a9bec4a0fe13b011efec68f9674fb1ad008f22b1
mansisharmanextuple <mansi.sharma@nextuple.com>
2022-08-26 11:32:03 +0530

    PR comment

--- common-configuration/common-configuration-near-cache-spring/src/main/java/com/hbc/common/config/spring/cache/feign/CommonConfigFeignImpl.java
+++ common-configuration/common-configuration-near-cache-spring/src/main/java/com/hbc/common/config/spring/cache/feign/CommonConfigFeignImpl.java
@@ -1,16 +1,11 @@
 package com.hbc.common.config.spring.cache.feign;
 
 import com.hbc.common.configuration.api.domain.dto.CommonConfigurationDto;
-import com.hbc.common.configuration.api.domain.inbound.CreateCommonConfigurationRequest;
 import com.hbc.common.response.BaseResponse;
 import com.hbc.core.cache.service.GenericFeignService;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
@@ -26,13 +21,4 @@ public interface CommonConfigFeignImpl
   @GetMapping("common-config/orgId/{orgId}")
   BaseResponse<CommonConfigurationDto> fetchValue(
       @PathVariable String orgId, @RequestParam String type, @RequestParam String key);
-  @PostMapping("common-config/")
-  BaseResponse<CommonConfigurationDto> createCommonConfiguration(
-          @RequestBody CreateCommonConfigurationRequest baseRequest);
-  @PutMapping("common-config/")
-  BaseResponse<CommonConfigurationDto> updateCommonConfiguration(
-          @RequestBody CreateCommonConfigurationRequest baseRequest);
-  @DeleteMapping("common-config/orgId/{orgId}")
-  BaseResponse<CommonConfigurationDto> deleteCommonConfiguration(
-          @PathVariable String orgId, @RequestParam String type, @RequestParam String key);
 }
