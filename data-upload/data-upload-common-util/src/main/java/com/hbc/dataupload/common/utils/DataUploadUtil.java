4f8175dfa9715bc5c27a30a83c35434db5dcb5de
Dave, Jimmy <jimmy.dave-vendor@hbc.com>
2022-09-02 11:13:51 +0530

    Added null check for action column

--- data-upload/data-upload-common-util/src/main/java/com/hbc/dataupload/common/utils/DataUploadUtil.java
+++ data-upload/data-upload-common-util/src/main/java/com/hbc/dataupload/common/utils/DataUploadUtil.java
@@ -61,7 +61,7 @@ public class DataUploadUtil {
       }
       actions.remove("action");
       for (Object action : actions) {
-        if (!(action.toString().equalsIgnoreCase(UPDATE_U)
+        if (action == null || !(action.toString().equalsIgnoreCase(UPDATE_U)
             || action.toString().equalsIgnoreCase(DELETE_D))) {
           throw new CommonServiceException(
               ACTION_INVALID_MESSAGE, HttpStatus.BAD_REQUEST, 0x1777, null);
