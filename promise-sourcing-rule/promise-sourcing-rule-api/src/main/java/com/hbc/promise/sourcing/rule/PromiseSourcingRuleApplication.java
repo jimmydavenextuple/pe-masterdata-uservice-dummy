348ee238cef8038010596b13097236deeef70e36
Chopra-vendor, Puneet <puneet.chopra-vendor@hbc.com>
2022-09-20 12:32:12 -0400

    Added validation for creating and updating promise sourcing rule. (#189)
    Co-authored-by: puneet-chopra-vendor_bay <somya-goe-vendor_bay>
    Co-authored-by: Goe, Somya <somya.goe-vendor@hbc.com>

--- promise-sourcing-rule/promise-sourcing-rule-api/src/main/java/com/hbc/promise/sourcing/rule/PromiseSourcingRuleApplication.java
+++ promise-sourcing-rule/promise-sourcing-rule-api/src/main/java/com/hbc/promise/sourcing/rule/PromiseSourcingRuleApplication.java
@@ -2,12 +2,14 @@ package com.hbc.promise.sourcing.rule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.hbc")
 @EnableJpaRepositories("com.hbc.promise.sourcing.rule")
+@EnableFeignClients(basePackages = {"com.hbc"})
 public class PromiseSourcingRuleApplication {
 
   public static void main(String[] args) {
