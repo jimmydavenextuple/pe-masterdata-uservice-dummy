0fa2607b87a92e62ec0794287c2f23b697ff5b9c
Sridhar S <sridhar.s@nextuple.com>
2022-09-22 12:41:41 +0530

    Refactored code for removing code duplications.

--- data-upload/csv-utility-common-util/src/main/java/com/hbc/csvdownload/common/pojo/TransitDataUpload.java
+++ data-upload/csv-utility-common-util/src/main/java/com/hbc/csvdownload/common/pojo/TransitDataUpload.java
@@ -1,49 +1,13 @@
 package com.hbc.csvdownload.common.pojo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import java.util.Date;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import com.hbc.transit.domain.inbound.TransitDataCreationRequest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TransitDataUpload {
-  @NotBlank(message = "orgId can't be blank")
-  @Length(max = 50)
-  private String orgId;
-
-  @NotBlank(message = "carrierServiceId can't be blank")
-  @Length(max = 50)
-  private String carrierServiceId;
-
-  @NotBlank(message = "sourceGeozone can't be blank")
-  @Length(max = 50)
-  private String sourceGeozone;
-
-  @NotBlank(message = "destinationGeozone can't be blank")
-  @Length(max = 50)
-  private String destinationGeozone;
-
-  @Min(value = 0, message = "transitDays can't be negative")
-  @NotNull(message = "transitDays can't be null")
-  private Float transitDays;
-
+public class TransitDataUpload extends TransitDataCreationRequest {
   private String actionType;
-
-  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
-  private Date bufferStartDate;
-
-  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
-  private Date bufferEndDate;
-
-  private Double bufferDays;
 }
