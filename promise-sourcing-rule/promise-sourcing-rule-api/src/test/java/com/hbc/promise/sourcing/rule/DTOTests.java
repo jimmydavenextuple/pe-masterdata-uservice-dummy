852979872225dad78dcab2a2ab387dc6930c3080
somya <somya.goel@nextuple.com>
2022-09-30 22:53:02 +0530

    fixing test cases

--- promise-sourcing-rule/promise-sourcing-rule-api/src/test/java/com/hbc/promise/sourcing/rule/DTOTests.java
+++ promise-sourcing-rule/promise-sourcing-rule-api/src/test/java/com/hbc/promise/sourcing/rule/DTOTests.java
@@ -6,6 +6,8 @@ import com.openpojo.validation.ValidatorBuilder;
 import com.openpojo.validation.test.impl.GetterTester;
 import com.openpojo.validation.test.impl.SetterTester;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 
 class DTOTests {
@@ -20,6 +22,7 @@ class DTOTests {
         .with(new GetterTester())
         .with(new SetterTester())
         .build()
-        .validate(pojoClassList);
+            .validate(pojoClassList.stream()
+                    .filter(x -> !x.getName().startsWith("com.hbc.postgres.config")).collect(Collectors.toList()));
   }
 }
