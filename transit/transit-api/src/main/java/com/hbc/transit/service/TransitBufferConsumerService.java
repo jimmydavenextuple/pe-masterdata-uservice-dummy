3bd9ab35781e91e6a8389c2dd199d0c6efeb5018
somya <somya.goel@nextuple.com>
2022-10-17 19:01:37 +0530

    EOD commit

---
+++ transit/transit-api/src/main/java/com/hbc/transit/service/TransitBufferConsumerService.java
@@ -0,0 +1,62 @@
+package com.hbc.transit.service;
+
+import com.hbc.common.exception.CommonServiceException;
+import com.hbc.jobs.framework.common.domain.enums.JobTypeEnum;
+import com.hbc.jobs.framework.common.domain.pojo.JobDetailsDto;
+import com.hbc.transit.domain.entity.TransitBufferConfigRequestEntity;
+import com.hbc.transit.domain.enums.TransitBufferConfigRequestStatusEnum;
+import com.hbc.transit.domain.enums.TransitBufferReqJobRefEnum;
+import com.hbc.transit.domain.outbound.TransitBufferReqJobRefResponse;
+import com.hbc.transit.exception.TransitBufferReqJobRefDomainException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class TransitBufferConsumerService {
+
+  private final TransitBufferConfigRequestService transitBufferConfigRequestService;
+  private final TransitBufferReqJobRefService transitBufferReqJobRefService;
+
+  public void processJobRecordForTransitBuffer(JobDetailsDto jobDetailsDto)
+      throws TransitBufferReqJobRefDomainException, CommonServiceException {
+    if (jobDetailsDto.getJobType().equals(JobTypeEnum.TRANSIT_BUFFER_REQUEST)) {
+      TransitBufferReqJobRefResponse transitBufferReqJobRefResponse =
+          transitBufferReqJobRefService
+              .getTransitBufferReqJobRefByExtReferenceId(jobDetailsDto.getJobId())
+              .get(0);
+      TransitBufferConfigRequestEntity transitBufferConfigRequest =
+          transitBufferConfigRequestService.getTransitBufferRequest(
+              transitBufferReqJobRefResponse.getTransitBufferReqId());
+      switch (jobDetailsDto.getStatus()) {
+        case COMPLETED:
+          {
+            if (transitBufferReqJobRefResponse.getAction().equals(TransitBufferReqJobRefEnum.CREATE)
+                || transitBufferReqJobRefResponse
+                    .getAction()
+                    .equals(TransitBufferReqJobRefEnum.UPDATE))
+              transitBufferConfigRequestService.updateTransitBufferRequestStatus(
+                  transitBufferConfigRequest.getId(),
+                  TransitBufferConfigRequestStatusEnum.COMPLETED);
+            if (transitBufferReqJobRefResponse
+                .getAction()
+                .equals(TransitBufferReqJobRefEnum.DELETE))
+              transitBufferConfigRequestService.updateTransitBufferRequestStatus(
+                  transitBufferConfigRequest.getId(), TransitBufferConfigRequestStatusEnum.DELETED);
+            break;
+          }
+
+        case FAILED:
+          {
+            transitBufferConfigRequestService.updateTransitBufferRequestStatus(
+                transitBufferConfigRequest.getId(), TransitBufferConfigRequestStatusEnum.ERROR);
+            break;
+          }
+        default:
+          break;
+      }
+    }
+  }
+}
