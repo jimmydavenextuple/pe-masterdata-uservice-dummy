9bd10ccc9ade80d619b84f2afcb612df9cfdbe45
Sahil-Nextuple <sahil.naikwadi@nextuple.com>
2023-06-09 12:18:15 +0530

    kafka changes

---
+++ src/main/java/com/nextuple/config/KafkaConsumerConfigs.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2022., Nextuple, Inc. and/or its affiliates. All rights reserved.
+ *
+ * The software, code and related documentation made available to you by Nextuple, Inc. are provided under a written agreement containing restrictions on use and disclosure and are protected by copyright and other intellectual property laws. As described in and unless expressly permitted in your agreement, you may not use, copy, reproduce, translate, broadcast, modify, license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or de-compilation of this software, unless required by law or permitted via contract for interoperability, is strictly prohibited.
+ * The information contained herein is subject to change without notice and is not warranted to be error-free. If you find any errors, please report them to us in writing.
+ */
+
+package com.nextuple.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.SerializationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+@EnableKafka
+@RequiredArgsConstructor
+public class KafkaConsumerConfigs {
+
+  @Value(value = "${spring.kafka.listener.concurrency:1}")
+  private Integer listenerConcurrency;
+
+  @Value(value = "${spring.kafka.consumer-item.key-deserializer}")
+  private String keyDeserializer;
+
+  @Value(value = "${spring.kafka.consumer-item.value-deserializer}")
+  private String valueDeserializer;
+
+  @Value(value = "${spring.kafka.bootstrap-servers}")
+  private String bootstrapServers;
+
+  @Value(value = "${spring.kafka.consumer-item.enable-auto-commit}")
+  private Boolean enableAutoCommit;
+
+  @Value(value = "${spring.kafka.consumer-item.auto-offset-reset}")
+  private String autoOffsetReset;
+
+  @Value(value = "${spring.kafka.consumer-item.properties.interceptor.classes}")
+  private String interceptorClasses;
+
+  private final KafkaProperties kafkaProperties;
+
+  @Primary
+  @Bean("jsonDeserializerProperties")
+  @ConfigurationProperties(prefix = "spring.kafka.consumer")
+  public Map<String, Object> jsonDeserializerProperties() {
+    return this.kafkaProperties.buildConsumerProperties();
+  }
+
+  @Bean
+  public ConsumerFactory<Object, Object> itemConsumerFactory() {
+    HashMap<String, Object> prop = new HashMap<>();
+    prop.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
+    prop.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
+    prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    prop.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
+    prop.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+    prop.put(ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptorClasses);
+    return new DefaultKafkaConsumerFactory<>(prop);
+  }
+
+  @Bean
+  @Primary
+  public ConsumerFactory<Object, Object> jsonConsumerFactory() {
+    HashMap<String, Object> prop = new HashMap<>(jsonDeserializerProperties());
+    Map<String, Object> properties = (Map<String, Object>) prop.get("properties");
+    prop.put(
+        ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS,
+        properties.get("spring-deserializer-value-delegate-class"));
+    prop.put(JsonDeserializer.TRUSTED_PACKAGES, properties.get("spring-json-trusted-packages"));
+    return new DefaultKafkaConsumerFactory<>(prop);
+  }
+
+  @Primary
+  @Bean(name = "JsonDeserializerConsumer")
+  public ConcurrentKafkaListenerContainerFactory<Object, Object> jsonKafkaContainerListenerFactory(
+      ConsumerFactory<Object, Object> consumerFactory,
+      KafkaOperations<Object, Object> kafkaOperations) {
+    ConcurrentKafkaListenerContainerFactory<Object, Object> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(jsonConsumerFactory());
+    return factory;
+  }
+
+  @Bean(name = "ItemDeserializerConsumer")
+  public ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaContainerListenerFactory(
+      ConsumerFactory<Object, Object> consumerFactory,
+      KafkaOperations<Object, Object> kafkaOperations) {
+    ConcurrentKafkaListenerContainerFactory<Object, Object> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(itemConsumerFactory());
+    factory.setConcurrency(listenerConcurrency);
+    factory.setCommonErrorHandler(kafkaErrorHandler(kafkaOperations));
+    return factory;
+  }
+
+  @Bean
+  public CommonErrorHandler kafkaErrorHandler(KafkaOperations<Object, Object> kafkaOperations) {
+    var defaultErrorHandler =
+        new DefaultErrorHandler(
+            new DeadLetterPublishingRecoverer(
+                kafkaOperations,
+                (cr, e) -> new TopicPartition(cr.topic() + ".err", cr.partition())));
+    defaultErrorHandler.addNotRetryableExceptions(
+        SerializationException.class, ClassCastException.class,
+        OptimisticLockingFailureException.class, NullPointerException.class);
+
+    return defaultErrorHandler;
+  }
+}
---
+++ src/main/java/com/nextuple/config/KafkaProducerConfigs.java
@@ -0,0 +1,97 @@
+package com.nextuple.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+@EnableKafka
+@RequiredArgsConstructor
+public class KafkaProducerConfigs {
+  private final KafkaProperties kafkaProperties;
+
+  @Bean("jsonSerializerProperties")
+  @ConfigurationProperties(prefix = "spring.kafka.producer-json")
+  public Map<String, Object> jsonSerializerProperties() {
+    return this.kafkaProperties.buildProducerProperties();
+  }
+
+  @Bean("itemSerializerProperties")
+  @ConfigurationProperties(prefix = "spring.kafka.producer-item")
+  public Map<String, Object> itemSerializerProperties() {
+    return this.kafkaProperties.buildProducerProperties();
+  }
+
+  @Bean
+  public ProducerFactory<String, Object> kafkaJsonProducerFactory() {
+    Map<String, Object> prop = new HashMap<>(jsonSerializerProperties());
+    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, prop.get("key-serializer"));
+    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, prop.get("value-serializer"));
+    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, prop.get("bootstrap-servers"));
+    Map<String, Object> properties = (Map<String, Object>) prop.get("properties");
+    Map<String, Object> interceptor = (Map<String, Object>) properties.get("interceptor");
+    Map<String, Object> saslProps = (Map<String, Object>) properties.get("sasl");
+
+    prop.put(ProducerConfig.ACKS_CONFIG, properties.get("acks").toString());
+    prop.put(CommonClientConfigs.RETRIES_CONFIG, properties.get("retries"));
+    prop.put(CommonClientConfigs.RETRY_BACKOFF_MS_CONFIG, properties.get("retry-backoff-ms"));
+
+    prop.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptor.get("classes"));
+    prop.put(
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG,
+        ((Map<String, Object>) properties.get("security"))
+            .get("protocol")); // properties -> security -> protocol
+    prop.put(SaslConfigs.SASL_MECHANISM, saslProps.get("mechanism"));
+    prop.put(
+        SaslConfigs.SASL_JAAS_CONFIG, ((Map<String, Object>) saslProps.get("jaas")).get("config"));
+    return new DefaultKafkaProducerFactory<>(prop);
+  }
+
+  @Bean
+  public ProducerFactory<Object, Object> kafkaItemProducerFactory() {
+    HashMap<String, Object> prop = new HashMap<>(itemSerializerProperties());
+    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, prop.get("key-serializer"));
+    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, prop.get("value-serializer"));
+    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, prop.get("bootstrap-servers"));
+    prop.put("schema.registry.url", prop.get("schema-registry-url"));
+    Map<String, Object> properties = (Map<String, Object>) prop.get("properties");
+    Map<String, Object> interceptor = (Map<String, Object>) properties.get("interceptor");
+    Map<String, Object> saslProps = (Map<String, Object>) properties.get("sasl");
+
+    prop.put(ProducerConfig.ACKS_CONFIG, properties.get("acks").toString());
+    prop.put(CommonClientConfigs.RETRIES_CONFIG, properties.get("retries"));
+    prop.put(CommonClientConfigs.RETRY_BACKOFF_MS_CONFIG, properties.get("retry-backoff-ms"));
+    prop.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptor.get("classes"));
+    prop.put(
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG,
+        ((Map<String, Object>) properties.get("security"))
+            .get("protocol")); // properties -> security -> protocol
+    prop.put(SaslConfigs.SASL_MECHANISM, saslProps.get("mechanism"));
+    prop.put(
+        SaslConfigs.SASL_JAAS_CONFIG, ((Map<String, Object>) saslProps.get("jaas")).get("config"));
+    return new DefaultKafkaProducerFactory<>(prop);
+  }
+
+  @Bean(name = "JsonSerializerProducer")
+  public KafkaTemplate<String, Object> kafkaTemplate() {
+    return new KafkaTemplate<>(kafkaJsonProducerFactory());
+  }
+
+  @Bean(name = "ItemSerializerProducer")
+  @Primary
+  public KafkaTemplate<Object, Object> avroKafkaTemplate() {
+    return new KafkaTemplate<>(kafkaItemProducerFactory());
+  }
+}
