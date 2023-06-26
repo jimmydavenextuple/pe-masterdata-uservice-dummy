5f397af300ba0b7aa067242822cbf34bf56a00a5
Sahil-Nextuple <sahil.naikwadi@nextuple.com>
2023-06-26 12:24:35 +0530

    kafka producer changes

--- src/main/java/com/nextuple/config/KafkaProducerConfigs.java
+++ src/main/java/com/nextuple/config/KafkaProducerConfigs.java
@@ -1,5 +1,6 @@
 package com.nextuple.config;
 
+import com.nextuple.core.event.LocalCacheUpdateEvent;
 import com.nextuple.jobs.framework.common.domain.pojo.RecordDto;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,7 +30,7 @@ public class KafkaProducerConfigs {
   private String bootstrapServers;
 
   @Bean("jsonSerializerProperties")
-  @ConfigurationProperties(prefix = "spring.kafka.producer-json")
+  @ConfigurationProperties(prefix = "spring.kafka.producer")
   public Map<String, Object> jsonSerializerProperties() {
     return this.kafkaProperties.buildProducerProperties();
   }
@@ -42,42 +43,21 @@ public class KafkaProducerConfigs {
 
   @Bean
   public ProducerFactory<String, RecordDto> kafkaJsonProducerFactory() {
-    Map<String, Object> prop = new HashMap<>(jsonSerializerProperties());
-    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, prop.get(KEYSERIALIZER));
-    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, prop.get(VALUESERIALIZER));
-    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-    prop.put(ProducerConfig.ACKS_CONFIG, prop.get("acks").toString());
-
-    prop.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, prop.get(INTERCEPTORCLASSES));
+    Map<String, Object> prop = getStringObjectMap();
 
     return new DefaultKafkaProducerFactory<>(prop);
   }
 
   @Bean
   public ProducerFactory<Object, Object> kafkaJsonProducerFactoryObj() {
-    Map<String, Object> prop = new HashMap<>(jsonSerializerProperties());
-    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, prop.get(KEYSERIALIZER));
-    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, prop.get(VALUESERIALIZER));
-    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-    prop.put(ProducerConfig.ACKS_CONFIG, prop.get("acks").toString());
-
-    prop.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, prop.get(INTERCEPTORCLASSES));
+    Map<String, Object> prop = getStringObjectMap();
 
     return new DefaultKafkaProducerFactory<>(prop);
   }
 
   @Bean
   public ProducerFactory<String, Object> kafkaJsonProducerFactoryStr() {
-    Map<String, Object> prop = new HashMap<>(jsonSerializerProperties());
-    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, prop.get(KEYSERIALIZER));
-    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, prop.get(VALUESERIALIZER));
-    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-    prop.put(ProducerConfig.ACKS_CONFIG, prop.get("acks").toString());
-
-    prop.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, prop.get(INTERCEPTORCLASSES));
+    Map<String, Object> prop = getStringObjectMap();
 
     return new DefaultKafkaProducerFactory<>(prop);
   }
@@ -92,7 +72,31 @@ public class KafkaProducerConfigs {
     return new DefaultKafkaProducerFactory<>(prop);
   }
 
+  @Bean
+  public ProducerFactory<String, LocalCacheUpdateEvent> kafkaJsonProducerFactoryEventListener() {
+    Map<String, Object> prop = getStringObjectMap();
+
+    return new DefaultKafkaProducerFactory<>(prop);
+  }
+
+  private Map<String, Object> getStringObjectMap() {
+    Map<String, Object> prop = new HashMap<>(jsonSerializerProperties());
+    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, prop.get(KEYSERIALIZER));
+    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, prop.get(VALUESERIALIZER));
+    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+    prop.put(ProducerConfig.ACKS_CONFIG, prop.get("acks").toString());
+
+    prop.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, prop.get(INTERCEPTORCLASSES));
+    return prop;
+  }
+
   @Primary
+  @Bean
+  public KafkaTemplate<String, LocalCacheUpdateEvent> kafkaTemplateEventListerner() {
+    return new KafkaTemplate<>(kafkaJsonProducerFactoryEventListener());
+  }
+
   @Bean(name = "JsonSerializerProducer")
   public KafkaTemplate<String, RecordDto> kafkaTemplate() {
     return new KafkaTemplate<>(kafkaJsonProducerFactory());
--- src/test/java/com/nextuple/item/TestUtil.java
+++ src/test/java/com/nextuple/item/TestUtil.java
@@ -204,4 +204,28 @@ public class TestUtil {
     serviceOptionEligibilities.put(NEXT_DAY_ELIGIBLE_FOR_DC, false);
     return serviceOptionEligibilities;
   }
+
+  public Map<String, Object> getJsonProps() {
+    Map<String, Object> props = new HashMap<>();
+    props.put("key-deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    props.put(
+        "value-deserializer",
+        "org.springframework.kafka.support.serializer.ErrorHandlingDeserializer");
+    props.put("bootstrap-servers", "localhost:90924");
+    props.put("enable-auto-commit", "true");
+    props.put("auto-offset-reset", "latest");
+    Map<String, Object> properties = new HashMap<>();
+    properties.put(
+        "spring-deserializer-value-delegate-class",
+        "org.springframework.kafka.support.serializer.JsonDeserializer");
+    properties.put("spring-json-trusted-packages", "*");
+    Map<String, Object> saslProps = new HashMap<>();
+    saslProps.put("mechanism", "SCRAM-SHA-512");
+    saslProps.put("jaas", Map.of("config", "exampleConfig"));
+    properties.put("sasl", saslProps);
+    properties.put("security", Map.of("protocol", "SASL_SSL"));
+    props.put("properties", properties);
+
+    return props;
+  }
 }
