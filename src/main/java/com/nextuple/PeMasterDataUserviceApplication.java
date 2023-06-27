1b6da09eded3ad4ddfa3ad3ac224e0eae6823945
rithik <rithik.sachdev-vendor@thebay.com>
2023-06-27 17:34:25 +0530

    fix spring application

--- src/main/java/com/nextuple/PeMasterDataUserviceApplication.java
+++ src/main/java/com/nextuple/PeMasterDataUserviceApplication.java
@@ -7,18 +7,25 @@
 
 package com.nextuple;
 
+import com.nextuple.pe.webhook.config.WebhookConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan("com.nextuple")
 @EntityScan("com.nextuple")
 @EnableJpaRepositories(basePackages = {"com.nextuple"})
 @EnableFeignClients(basePackages = {"com.nextuple"})
+@EnableConfigurationProperties({WebhookConfiguration.class})
+@EnableScheduling
+@Slf4j
 public class PeMasterDataUserviceApplication {
 
   public static void main(String[] args) {
