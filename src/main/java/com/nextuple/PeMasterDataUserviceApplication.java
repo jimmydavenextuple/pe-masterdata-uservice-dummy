d4f191b12b85bc91e2ce7bb417f2933f47990709
rithik <rithik.sachdev-vendor@thebay.com>
2023-06-27 16:28:56 +0530

    fix entity bean missing error

--- src/main/java/com/nextuple/PeMasterDataUserviceApplication.java
+++ src/main/java/com/nextuple/PeMasterDataUserviceApplication.java
@@ -9,12 +9,14 @@ package com.nextuple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.nextuple")
+@EntityScan("com.nextuple.*")
 @EnableJpaRepositories(basePackages = {"com.nextuple"})
 @EnableFeignClients(basePackages = {"com.nextuple"})
 public class PeMasterDataUserviceApplication {
