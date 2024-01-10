8340e9555e52d4752bfdd013855b683ce7855594
goutham.sekar@nextuple.com <goutham.sekar@nextuple.com>
2024-01-11 01:42:20 +0530

    Updated Integration Env

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM eclipse-temurin:21-jre-alpine
 VOLUME /tmp
 COPY "./build/libs/*[^plain].jar" app.jar
 EXPOSE 8080
+COPY jacocoAgent.jar agent.jar
+COPY jacococli.jar cli.jar
 COPY ./entrypoint.sh entrypoint.sh
 ENTRYPOINT ["/bin/sh","entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 OPTS="-server -XX:+UseStringDeduplication -Xlog:gc*,gc+phases=debug:file=gc.log:time,pid,tags:filecount=10,filesize=20m ${CUSTOM_JAVA_OPTS}"
-java -jar $OPTS app.jar
+java -javaagent:/agent.jar=address=localhost,port=6300,output=tcpclient -jar  $OPTS app.jar
