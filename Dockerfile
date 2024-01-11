07ab704db12c424b6f963c63e2bda88f5d79a98d
goutham.sekar@nextuple.com <goutham.sekar@nextuple.com>
2024-01-11 09:06:10 +0530

    Updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY "./build/libs/*[^plain].jar" app.jar
 EXPOSE 8080
 COPY jacocoAgent.jar agent.jar
 COPY jacococli.jar cli.jar
+COPY jacocoserver.jar server.jar
 COPY ./entrypoint.sh entrypoint.sh
 ENTRYPOINT ["/bin/sh","entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 OPTS="-server -XX:+UseStringDeduplication -Xlog:gc*,gc+phases=debug:file=gc.log:time,pid,tags:filecount=10,filesize=20m ${CUSTOM_JAVA_OPTS}"
-java -javaagent:/agent.jar=address=localhost,port=6300,output=tcpclient -jar  $OPTS app.jar
+java -jar server.jar & java -javaagent:/agent.jar=address=localhost,port=6300,output=tcpclient -jar  $OPTS app.jar
