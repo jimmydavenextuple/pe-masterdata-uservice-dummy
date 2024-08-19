4c10cb17e0b896e29af6153b89909de4982efe72
Abhishek Singhal <abhishek.singhal@nextuple.com>
2024-08-19 10:07:53 +0000

    Merged in PSE-2635-Add-joda-time-dependency (pull request #128)
    Add jodaModule to object mapper
    
    * Add jodaModule to object mapper
    
    * Add joda jackson dependency
    
    * Add unit test for mapper config
    
    
    Approved-by: Pradeep
    Approved-by: Sridhar

---
+++ src/test/java/com/nextuple/masterdata/config/MapperConfigTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024., Nextuple, Inc. and/or its affiliates. All rights reserved.
+ *
+ * The software, code and related documentation made available to you by Nextuple, Inc. are provided under a written agreement containing restrictions on use and disclosure and are protected by copyright and other intellectual property laws. As described in and unless expressly permitted in your agreement, you may not use, copy, reproduce, translate, broadcast, modify, license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or de-compilation of this software, unless required by law or permitted via contract for interoperability, is strictly prohibited.
+ * The information contained herein is subject to change without notice and is not warranted to be error-free. If you find any errors, please report them to us in writing.
+ */
+
+package com.nextuple.masterdata.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+class MapperConfigTest {
+  @InjectMocks MapperConfig mapperConfig;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void mapperTest() {
+    ObjectMapper mapper = mapperConfig.mapper();
+    Assertions.assertEquals(mapper.getClass(), ObjectMapper.class);
+  }
+}
