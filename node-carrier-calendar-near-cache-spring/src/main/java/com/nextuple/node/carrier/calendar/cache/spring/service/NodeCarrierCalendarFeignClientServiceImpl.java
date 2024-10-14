8a945ac9fac1553b5028f406ca08382f3806a981
kiranpoornavishnu <kiran.vishnu@nextuple.com>
2024-10-14 10:20:34 +0530

    commiting missed node-carrier-calendar-near-cache-spring module

---
+++ node-carrier-calendar-near-cache-spring/src/main/java/com/nextuple/node/carrier/calendar/cache/spring/service/NodeCarrierCalendarFeignClientServiceImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022., Nextuple, Inc. and/or its affiliates. All rights reserved.
+ *
+ * The software, code and related documentation made available to you by Nextuple, Inc. are provided under a written agreement containing restrictions on use and disclosure and are protected by copyright and other intellectual property laws. As described in and unless expressly permitted in your agreement, you may not use, copy, reproduce, translate, broadcast, modify, license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or de-compilation of this software, unless required by law or permitted via contract for interoperability, is strictly prohibited.
+ * The information contained herein is subject to change without notice and is not warranted to be error-free. If you find any errors, please report them to us in writing.
+ */
+
+package com.nextuple.node.carrier.calendar.cache.spring.service;
+
+import com.nextuple.calendar.common.CalendarCommonFeignImpl;
+import com.nextuple.calendar.domain.CalendarDaysStatusInfo;
+import com.nextuple.common.response.BaseResponse;
+import com.nextuple.core.cache.mapper.GenericMapper;
+import com.nextuple.core.spring.service.AbstractGenericFeignClientServiceImpl;
+import com.nextuple.node.carrier.calendar.cache.domain.NodeCarrierCalendarCacheKey;
+import com.nextuple.node.carrier.calendar.cache.domain.NodeCarrierCalendarCacheValue;
+import java.util.List;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+// Added this
+@AllArgsConstructor
+public class NodeCarrierCalendarFeignClientServiceImpl
+    extends AbstractGenericFeignClientServiceImpl<
+        NodeCarrierCalendarCacheKey,
+        NodeCarrierCalendarCacheValue,
+        String,
+        BaseResponse<List<CalendarDaysStatusInfo>>> {
+
+  @Autowired CalendarCommonFeignImpl calendarCommonFeign;
+
+  @Autowired
+  GenericMapper<
+          NodeCarrierCalendarCacheKey,
+          NodeCarrierCalendarCacheValue,
+          String,
+          BaseResponse<List<CalendarDaysStatusInfo>>>
+      nodeCalendarMapper;
+
+  @Override
+  public NodeCarrierCalendarCacheValue get(NodeCarrierCalendarCacheKey key) {
+    try {
+      BaseResponse<List<CalendarDaysStatusInfo>> response =
+          calendarCommonFeign.getNodeCarrierCalendar(
+              key.getOrgId(),
+              key.getNodeId(),
+              key.getCarrierServiceId(),
+              key.getServiceOption(),
+              key.getFromDate());
+
+      if (Objects.isNull(response.getPayload())) {
+        return NodeCarrierCalendarCacheValue.builder().calendarDaysStatusInfo(null).build();
+      }
+      return nodeCalendarMapper.responseToCacheValue(response);
+    } catch (RuntimeException e) {
+      return NodeCarrierCalendarCacheValue.builder().calendarDaysStatusInfo(null).build();
+    }
+  }
+}
---
+++ node-carrier-calendar-near-cache-spring/src/test/java/com/nextuple/node/carrier/calendar/cache/spring/util/TestUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2022., Nextuple, Inc. and/or its affiliates. All rights reserved.
+ *
+ * The software, code and related documentation made available to you by Nextuple, Inc. are provided under a written agreement containing restrictions on use and disclosure and are protected by copyright and other intellectual property laws. As described in and unless expressly permitted in your agreement, you may not use, copy, reproduce, translate, broadcast, modify, license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or de-compilation of this software, unless required by law or permitted via contract for interoperability, is strictly prohibited.
+ * The information contained herein is subject to change without notice and is not warranted to be error-free. If you find any errors, please report them to us in writing.
+ */
+
+package com.nextuple.node.carrier.calendar.cache.spring.util;
+
+import com.nextuple.calendar.domain.CalendarDaysStatusInfo;
+import com.nextuple.common.response.BaseResponse;
+import com.nextuple.node.carrier.calendar.cache.domain.NodeCarrierCalendarCacheKey;
+import com.nextuple.node.carrier.calendar.cache.domain.NodeCarrierCalendarCacheValue;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestUtil {
+
+  public static final String NODE_ID = "Node_id_01";
+  public static final String ORG_ID = "Org_Id_01";
+  public static final String CARRIER_SERVICE_ID = "Carrier_service_Id_01";
+  public static final String SERVICE_OPTION = "Service_Option_01";
+
+  public NodeCarrierCalendarCacheValue getNodeCarrierCalendarCacheValue() {
+    List<CalendarDaysStatusInfo> calendarDaysStatusInfoList = getCalendarDaysStatusInfo();
+    return NodeCarrierCalendarCacheValue.builder()
+        .calendarDaysStatusInfo(calendarDaysStatusInfoList)
+        .build();
+  }
+
+  public List<CalendarDaysStatusInfo> getCalendarDaysStatusInfo() {
+    List<CalendarDaysStatusInfo> calendarDaysStatusInfoList = new ArrayList<>();
+    CalendarDaysStatusInfo calendarDaysStatusInfo = new CalendarDaysStatusInfo();
+    calendarDaysStatusInfo.setDate("2022-07-07");
+    calendarDaysStatusInfo.setIsActive(true);
+    calendarDaysStatusInfoList.add(calendarDaysStatusInfo);
+    return calendarDaysStatusInfoList;
+  }
+
+  public BaseResponse<List<CalendarDaysStatusInfo>>
+      getBaseResponseOfListOfCalendarDaysStatusInfo() {
+    return BaseResponse.builder()
+        .message("Carrier calendar details fetched successfully")
+        .payload(getCalendarDaysStatusInfo())
+        .build();
+  }
+
+  public NodeCarrierCalendarCacheKey getNodeCarrierCalendarCacheKey() {
+    return NodeCarrierCalendarCacheKey.builder()
+        .nodeId(NODE_ID)
+        .orgId(ORG_ID)
+        .carrierServiceId(CARRIER_SERVICE_ID)
+        .serviceOption(SERVICE_OPTION)
+        .build();
+  }
+}
