91a32ba6f17538e1d3b1f490406004045eee8f3a
kiranpoornavishnu <kiran.vishnu@nextuple.com>
2024-10-17 18:22:10 +0530

    changes for upload service with build failing

---
+++ pe-data-upload-uservice/csv-utility-api/src/main/java/com/nextuple/csvdownload/domain/mapper/ZoneDataRequestMapper.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022., Nextuple, Inc. and/or its affiliates. All rights reserved.
+ *
+ * The software, code and related documentation made available to you by Nextuple, Inc. are provided under a written agreement containing restrictions on use and disclosure and are protected by copyright and other intellectual property laws. As described in and unless expressly permitted in your agreement, you may not use, copy, reproduce, translate, broadcast, modify, license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or de-compilation of this software, unless required by law or permitted via contract for interoperability, is strictly prohibited.
+ * The information contained herein is subject to change without notice and is not warranted to be error-free. If you find any errors, please report them to us in writing.
+ */
+
+package com.nextuple.csvdownload.domain.mapper;
+
+import com.nextuple.csvdownload.domain.pojo.ZoneDataErrorLogsPojo;
+import com.nextuple.jobs.framework.common.domain.pojo.ZoneDataUpload;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ZoneDataRequestMapper {
+
+  ZoneDataErrorLogsPojo convertToZoneDataErrorLogsPojo(ZoneDataUpload zoneDataUpload);
+}
