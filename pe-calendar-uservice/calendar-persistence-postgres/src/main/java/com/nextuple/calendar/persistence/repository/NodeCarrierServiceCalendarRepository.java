e16c603a1bb7e4417c8e27540110af7d8667ed7e
JeewanNextuple <jeewan.khatiwara@nextuple.com>
2025-01-22 12:06:30 +0530

    query updated to fix the download when effective date is same

--- pe-calendar-uservice/calendar-persistence-postgres/src/main/java/com/nextuple/calendar/persistence/repository/NodeCarrierServiceCalendarRepository.java
+++ pe-calendar-uservice/calendar-persistence-postgres/src/main/java/com/nextuple/calendar/persistence/repository/NodeCarrierServiceCalendarRepository.java
@@ -50,6 +50,10 @@ public interface NodeCarrierServiceCalendarRepository
   @Query(value = "SELECT * FROM node_carrier_service_calendars LIMIT ?1", nativeQuery = true)
   List<NodeCarrierServiceCalendarEntity> findAllNodeCarrierServiceCalendars(Integer limit);
 
+  @Query(
+      value =
+          "SELECT * FROM node_carrier_service_calendars WHERE org_id = ?1 ORDER BY created_date DESC",
+      nativeQuery = true)
   List<NodeCarrierServiceCalendarEntity> findAllNodeCarrierServiceCalendarsByOrgId(String orgId);
 
   List<NodeCarrierServiceCalendarEntity> findAllNodeCarrierServiceCalendarsByOrgIdAndNodeId(
