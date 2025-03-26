2d9582e9ff3803e1e305b848061a027178c78909
Vaibhav Singh <vaibhav.singh@nextuple.com>
2025-03-26 16:53:42 +0530

    Removed Conflicting field

--- pe-transit-uservice/transit-api-domain/src/main/java/com/nextuple/transit/domain/inbound/FetchTransferScheduleRequest.java
+++ pe-transit-uservice/transit-api-domain/src/main/java/com/nextuple/transit/domain/inbound/FetchTransferScheduleRequest.java
@@ -55,10 +55,4 @@ public class FetchTransferScheduleRequest implements Serializable {
 
   @Schema(description = "Rule Information for the transfer request.")
   private List<Pair<String, String>> ruleInfo;
-
-  @Schema(
-      description =
-          "Indicates whether filtration based on the sourcing attribute ID should be applied.",
-      example = "true/false")
-  private Boolean isSourcingAttributeEnabled;
 }
