e33ff682b60453997b47ee9d5f60734fa2132bce
Ravula Pranay Kumar Reddy <pranay.reddy@nextuple.com>
2025-03-31 21:53:11 +0530

    Revert "added firewall logging filter"
    This reverts commit c18add963802ddabba958e041e2f6d5a8e87ed51.

--- pe-common/common-util/src/main/java/com/nextuple/common/exception/FirewallExceptionLoggingFilter.java
+++ pe-common/common-util/src/main/java/com/nextuple/common/exception/FirewallExceptionLoggingFilter.java
@@ -7,14 +7,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class FirewallExceptionLoggingFilter extends OncePerRequestFilter {
   private static final Logger logger =
       LoggerFactory.getLogger(FirewallExceptionLoggingFilter.class);
@@ -26,11 +23,10 @@ public class FirewallExceptionLoggingFilter extends OncePerRequestFilter {
     try {
       filterChain.doFilter(request, response);
     } catch (RequestRejectedException ex) {
-      logger.error(
-          "Incoming request: {} {} {} with the following exception :{}",
-          request.getServletPath(),
-          request.getRequestURL(),
+      logger.info(
+          "Incoming request: {} {} with the following exception :{}",
           request.getMethod(),
+          request.getRequestURL(),
           ex.getCause());
       response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Blocked by security firewall");
     }
