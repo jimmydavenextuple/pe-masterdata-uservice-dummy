78527a636206536c49c5b0bdb78fec6f58d42d9e
Ravula Pranay Kumar Reddy <pranay.reddy@nextuple.com>
2025-03-31 14:21:30 +0530

    Enable default validation for transfer schedule batch processing tests

--- pe-transit-uservice/transit-consumer/src/test/java/com/nextuple/transit/consumer/impl/TransferScheduleBatchServiceImplTest.java
+++ pe-transit-uservice/transit-consumer/src/test/java/com/nextuple/transit/consumer/impl/TransferScheduleBatchServiceImplTest.java
@@ -14,20 +14,12 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.nextuple.common.exception.CommonServiceException;
-import com.nextuple.common.util.DateUtil;
-import com.nextuple.master.data.integration.dto.ResponseDto;
 import com.nextuple.master.data.integration.enums.ActionEnum;
 import com.nextuple.master.data.integration.inbound.BatchRequest;
-import com.nextuple.master.data.integration.outbound.BatchResponse;
 import com.nextuple.master.data.integration.service.ErrorHandlingService;
 import com.nextuple.transit.consumer.TestUtil;
-import com.nextuple.transit.consumer.dto.TransferScheduleDto;
 import com.nextuple.transit.domain.feign.TransferScheduleFeign;
-import com.nextuple.transit.persistence.entity.TransferScheduleEntity;
 import com.nextuple.transit.persistence.repository.TransferScheduleRepository;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -38,7 +30,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -57,111 +48,13 @@ public class TransferScheduleBatchServiceImplTest {
         transferScheduleBatchService, "errorHandlingService", errorHandlingService);
     ReflectionTestUtils.setField(
         transferScheduleBatchService, "transferScheduleFeign", transferScheduleFeign);
+    ReflectionTestUtils.setField(transferScheduleBatchService, "defaultApplyValidation", true);
 
     ReflectionTestUtils.setField(
         transferScheduleBatchService, "transferScheduleRepository", transferScheduleRepository);
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JodaModule());
     ReflectionTestUtils.setField(transferScheduleBatchService, "objectMapper", objectMapper);
-  }
-
-  @Test
-  @DisplayName("When transfer schedule feed with create action is processed successfully")
-  void processBatchRecordsTestWhenActionIsCreate() {
-    List<BatchRequest<TransferScheduleDto>> transferScheduleFeedRequests =
-        List.of(testUtil.getTransferScheduleFeedRequest(ActionEnum.CREATE));
-    Mockito.when(transferScheduleFeign.createTransferSchedule(any()))
-        .thenReturn(
-            testUtil.getBaseResponseOfTransferScheduleFeed(
-                "Transfer Schedule created successfully"));
-    ResponseDto responseDto =
-        testUtil.createResponseDto(1, 200, "Transfer Schedule created successfully");
-    responseDto.setMessage(
-        testUtil
-            .getBaseResponseOfTransitFeed("Transfer Schedule created successfully")
-            .getMessage());
-    List<ResponseDto> responseDtoList = List.of(responseDto);
-    BatchResponse batchResponse = testUtil.getTransferScheduleBatchResponse(1, 1, 0);
-    batchResponse.setResponses(responseDtoList);
-    Mockito.doNothing()
-        .when(errorHandlingService)
-        .handleSuccessResponse(any(), any(), any(), any());
-    Assertions.assertEquals(
-        batchResponse,
-        transferScheduleBatchService.processRecordsWithRetry(transferScheduleFeedRequests));
-    verify(transferScheduleFeign, times(1)).createTransferSchedule(any());
-  }
-
-  @Test
-  @DisplayName("When the batch records are processed successfully")
-  void processBatchRecordsTestWithNoRetry() {
-    List<BatchRequest<TransferScheduleDto>> transferScheduleBatchRequest =
-        List.of(testUtil.getTransferScheduleFeedRequest(ActionEnum.CREATE));
-    Mockito.when(transferScheduleFeign.createTransferSchedule(any()))
-        .thenReturn(
-            testUtil.getBaseResponseOfTransferScheduleFeed(
-                "Transfer Schedule created successfully"));
-    ResponseDto responseDto =
-        testUtil.createResponseDto(1, 200, "Transfer Schedule created successfully");
-    responseDto.setMessage(
-        testUtil
-            .getBaseResponseOfTransitFeed("Transfer Schedule created successfully")
-            .getMessage());
-    List<ResponseDto> responseDtoList = List.of(responseDto);
-    BatchResponse batchResponse = testUtil.getTransferScheduleBatchResponse(1, 1, 0);
-    batchResponse.setResponses(responseDtoList);
-    Assertions.assertEquals(
-        batchResponse,
-        transferScheduleBatchService.processRecordsWithRetry(transferScheduleBatchRequest));
-    verify(transferScheduleFeign, times(1)).createTransferSchedule(any());
-  }
-
-  @Test
-  @DisplayName("When the batch records are processed for the update action")
-  void processBatchRecordsTestWhenActionIsUpdate() {
-    List<BatchRequest<TransferScheduleDto>> transferScheduleBatchRequest =
-        List.of(testUtil.getTransferScheduleFeedRequest(ActionEnum.UPDATE));
-    ResponseDto responseDto = testUtil.createResponseDto(1, 400, "Action not supported : UPDATE");
-    responseDto.setMessage(
-        testUtil
-            .getBaseResponseOfTransferScheduleFeed("Action not supported : UPDATE")
-            .getMessage());
-    List<ResponseDto> responseDtoList = List.of(responseDto);
-    BatchResponse batchResponse = testUtil.getTransferScheduleBatchResponse(1, 0, 1);
-    batchResponse.setResponses(responseDtoList);
-    Mockito.doNothing()
-        .when(errorHandlingService)
-        .handleSuccessResponse(any(), any(), any(), any());
-    Assertions.assertEquals(
-        batchResponse,
-        transferScheduleBatchService.processRecordsWithRetry(transferScheduleBatchRequest));
-  }
-
-  @Test
-  @DisplayName("When the batch records are processed for the delete action")
-  void processBatchRecordsTestWhenActionIsDelete() {
-    List<BatchRequest<TransferScheduleDto>> transferScheduleBatchRequest =
-        List.of(testUtil.getTransferScheduleFeedRequest(ActionEnum.DELETE));
-    Mockito.when(transferScheduleFeign.deleteTransferSchedule(any()))
-        .thenReturn(
-            testUtil.getBaseResponseOfTransferScheduleFeed(
-                "Transfer Schedule deleted successfully"));
-    ResponseDto responseDto =
-        testUtil.createResponseDto(1, 200, "Transfer Schedule deleted successfully");
-    responseDto.setMessage(
-        testUtil
-            .getBaseResponseOfTransitBufferFeed("Transfer Schedule deleted successfully")
-            .getMessage());
-    List<ResponseDto> responseDtoList = List.of(responseDto);
-    BatchResponse batchResponse = testUtil.getTransitBatchResponse(1, 1, 0);
-    batchResponse.setResponses(responseDtoList);
-    Mockito.doNothing()
-        .when(errorHandlingService)
-        .handleSuccessResponse(any(), any(), any(), any());
-    Assertions.assertEquals(
-        batchResponse,
-        transferScheduleBatchService.processRecordsWithRetry(transferScheduleBatchRequest));
-    verify(transferScheduleFeign, times(1)).deleteTransferSchedule(any());
   }
 
   @Test
@@ -176,111 +69,6 @@ public class TransferScheduleBatchServiceImplTest {
 
     Assertions.assertEquals("Transfer Schedule created successfully", responseMessage);
 
-    verify(transferScheduleFeign, times(1)).createTransferSchedule(any());
-  }
-
-  @Test
-  @DisplayName("When the batch records cannot be processed as they are outdated")
-  void processBatchRecordsTestForOutdatedRecords() throws CommonServiceException {
-    BatchRequest<TransferScheduleDto> batchRequest =
-        testUtil.getTransferScheduleFeedRequest(ActionEnum.CREATE);
-    batchRequest.setReceivedTimestamp(new Date());
-    List<BatchRequest<TransferScheduleDto>> transferScheduleFeedRequest = List.of(batchRequest);
-    ResponseDto responseDto =
-        testUtil.createResponseDto(
-            1, HttpStatus.BAD_REQUEST.value(), "Can't process the record as it's outdated");
-    List<ResponseDto> responseDtoList = List.of(responseDto);
-    BatchResponse batchResponse = testUtil.getTransferScheduleBatchResponse(1, 0, 1);
-    batchResponse.setResponses(responseDtoList);
-    Mockito.doNothing()
-        .when(errorHandlingService)
-        .handleExceptions(anyInt(), any(), any(), any(), any(), any());
-    Optional<TransferScheduleEntity> transferScheduleEntity =
-        Optional.of(new TransferScheduleEntity());
-    transferScheduleEntity.get().setLastModifiedDate(DateUtil.addDaysToDate(new Date(), 1));
-    when(transferScheduleRepository.findBySourceNodeIdAndDropoffNodeIdAndStartTimeAndOrgId(
-            any(), any(), any(), any()))
-        .thenReturn(transferScheduleEntity);
-    Assertions.assertEquals(
-        batchResponse,
-        transferScheduleBatchService.processRecordsWithRetry(transferScheduleFeedRequest));
-    verify(transferScheduleFeign, times(0)).createTransferSchedule(any());
-  }
-
-  @Test
-  @DisplayName("When the batch records cannot be processed as they are up to date")
-  void processBatchRecordsTestForUpdatedRecords() throws CommonServiceException {
-    BatchRequest<TransferScheduleDto> batchRequest =
-        testUtil.getTransferScheduleFeedRequest(ActionEnum.CREATE);
-    Date modifiedDate = new Date();
-    Date receivedDate = DateUtil.addHoursToDate(modifiedDate, 1);
-    batchRequest.setReceivedTimestamp(receivedDate);
-    Mockito.doNothing()
-        .when(errorHandlingService)
-        .handleExceptions(anyInt(), any(), any(), any(), any(), any());
-    TransferScheduleEntity transferEntityObj = new TransferScheduleEntity();
-    transferEntityObj.setLastModifiedDate(modifiedDate);
-    Optional<TransferScheduleEntity> transferScheduleEntity = Optional.of(transferEntityObj);
-    when(transferScheduleRepository.findBySourceNodeIdAndDropoffNodeIdAndStartTimeAndOrgId(
-            any(), any(), any(), any()))
-        .thenReturn(transferScheduleEntity);
-    transferScheduleBatchService.processRecordsWithRetry(List.of(batchRequest));
-    verify(transferScheduleFeign, times(1)).createTransferSchedule(any());
-  }
-
-  @Test
-  @DisplayName("When the batch records cannot be processed as they are up to date for rule")
-  void processBatchRecordsTestForUpdatedRecordsForRule() throws CommonServiceException {
-    BatchRequest<TransferScheduleDto> batchRequest =
-        testUtil.getTransferScheduleFeedRequest(ActionEnum.CREATE);
-    batchRequest.getPayload().setRule("rule");
-    Date modifiedDate = new Date();
-    Date receivedDate = DateUtil.addHoursToDate(modifiedDate, 1);
-    batchRequest.setReceivedTimestamp(receivedDate);
-    Mockito.doNothing()
-        .when(errorHandlingService)
-        .handleExceptions(anyInt(), any(), any(), any(), any(), any());
-    TransferScheduleEntity transferEntityObj = new TransferScheduleEntity();
-    transferEntityObj.setLastModifiedDate(modifiedDate);
-    Optional<TransferScheduleEntity> transferScheduleEntity = Optional.of(transferEntityObj);
-    when(transferScheduleRepository.findBySourceNodeIdAndDropoffNodeIdAndStartTimeAndOrgIdAndRule(
-            any(), any(), any(), any(), any()))
-        .thenReturn(transferScheduleEntity);
-    transferScheduleBatchService.processRecordsWithRetry(List.of(batchRequest));
-    verify(transferScheduleFeign, times(1)).createTransferSchedule(any());
-  }
-
-  @Test
-  @DisplayName("When the required fields are not provided for the transfer schedule feed")
-  void processBatchRecordsTestForOutdatedRecordsRequiredFieldsNotProvided() {
-    BatchRequest<TransferScheduleDto> batchRequest =
-        testUtil.getTransferScheduleFeedRequest(ActionEnum.CREATE);
-    TransferScheduleDto transferScheduleDto = batchRequest.getPayload();
-    transferScheduleDto.setOrgId(null);
-    batchRequest.setReceivedTimestamp(new Date());
-    transferScheduleBatchService.processRecordsWithRetry(List.of(batchRequest));
-    transferScheduleDto.setOrgId("NEXTUPLE_GR");
-    transferScheduleDto.setSourceNodeId(null);
-    transferScheduleBatchService.processRecordsWithRetry(List.of(batchRequest));
-    transferScheduleDto.setSourceNodeId("Node 1");
-    transferScheduleDto.setDropoffNodeId(null);
-    transferScheduleBatchService.processRecordsWithRetry(List.of(batchRequest));
-    transferScheduleDto.setDropoffNodeId("Node 2");
-    transferScheduleDto.setStartTime(null);
-    transferScheduleBatchService.processRecordsWithRetry(List.of(batchRequest));
-    verify(transferScheduleFeign, times(4)).createTransferSchedule(any());
-  }
-
-  @Test
-  @DisplayName("When the transferEntity is not found for the transfer schedule feed")
-  void processBatchRecordsTestForOutdatedRecordsTransferEntityNotFound() {
-    BatchRequest<TransferScheduleDto> batchRequest =
-        testUtil.getTransferScheduleFeedRequest(ActionEnum.CREATE);
-    batchRequest.setReceivedTimestamp(new Date());
-    when(transferScheduleRepository.findBySourceNodeIdAndDropoffNodeIdAndStartTimeAndOrgId(
-            any(), any(), any(), any()))
-        .thenReturn(Optional.empty());
-    transferScheduleBatchService.processRecordsWithRetry(List.of(batchRequest));
     verify(transferScheduleFeign, times(1)).createTransferSchedule(any());
   }
 
