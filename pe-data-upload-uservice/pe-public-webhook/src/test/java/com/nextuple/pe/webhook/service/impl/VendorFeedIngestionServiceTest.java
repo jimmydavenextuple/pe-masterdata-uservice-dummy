b41820182d468880d489678276e44c9c84e61cdf
Vaibhav Singh <vaibhav.singh@nextuple.com>
2025-04-04 11:30:08 +0530

    Added copyright statements

--- pe-data-upload-uservice/pe-public-webhook/src/test/java/com/nextuple/pe/webhook/service/impl/VendorFeedIngestionServiceTest.java
+++ pe-data-upload-uservice/pe-public-webhook/src/test/java/com/nextuple/pe/webhook/service/impl/VendorFeedIngestionServiceTest.java
@@ -1,7 +1,20 @@
+/*
+ * Copyright (c) 2025., Nextuple, Inc. and/or its affiliates. All rights reserved.
+ *
+ * The software, code and related documentation made available to you by Nextuple, Inc. are provided under a written agreement containing restrictions on use and disclosure and are protected by copyright and other intellectual property laws. As described in and unless expressly permitted in your agreement, you may not use, copy, reproduce, translate, broadcast, modify, license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or de-compilation of this software, unless required by law or permitted via contract for interoperability, is strictly prohibited.
+ * The information contained herein is subject to change without notice and is not warranted to be error-free. If you find any errors, please report them to us in writing.
+ */
+
 package com.nextuple.pe.webhook.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.nextuple.master.data.integration.inbound.BatchRequest;
 import com.nextuple.pe.webhook.domain.dtos.MasterDataIngestionDto;
--- pe-vendor-uservice/vendor-persistence-domain/src/main/java/com/nextuple/vendor/persistence/domain/VendorDomainDto.java
+++ pe-vendor-uservice/vendor-persistence-domain/src/main/java/com/nextuple/vendor/persistence/domain/VendorDomainDto.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025., Nextuple, Inc. and/or its affiliates. All rights reserved.
+ *
+ * The software, code and related documentation made available to you by Nextuple, Inc. are provided under a written agreement containing restrictions on use and disclosure and are protected by copyright and other intellectual property laws. As described in and unless expressly permitted in your agreement, you may not use, copy, reproduce, translate, broadcast, modify, license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or de-compilation of this software, unless required by law or permitted via contract for interoperability, is strictly prohibited.
+ * The information contained herein is subject to change without notice and is not warranted to be error-free. If you find any errors, please report them to us in writing.
+ */
+
 package com.nextuple.vendor.persistence.domain;
 
 import com.nextuple.common.domain.DomainBaseEntity;
@@ -12,7 +19,7 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VendorDomainDto extends DomainBaseEntity implements Serializable {
-  private static final long serialVersionUID = 1240891589171888066L;
+  private static final long serialVersionUID = 1240891589171888076L;
   String vendorId;
   String orgId;
   String vendorDescription;
