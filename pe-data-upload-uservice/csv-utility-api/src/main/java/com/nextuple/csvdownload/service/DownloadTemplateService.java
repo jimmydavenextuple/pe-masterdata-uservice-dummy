a4d8ff9e7f14bb1a3c22cfd2d63fb45d3b27e543
Ravula Pranay Kumar Reddy <pranay.reddy@nextuple.com>
2025-04-14 14:03:42 +0530

    Refactor DownloadTemplateService to sort custom attributes and remove unused code

--- pe-data-upload-uservice/csv-utility-api/src/main/java/com/nextuple/csvdownload/service/DownloadTemplateService.java
+++ pe-data-upload-uservice/csv-utility-api/src/main/java/com/nextuple/csvdownload/service/DownloadTemplateService.java
@@ -18,11 +18,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
@@ -173,17 +169,23 @@ public class DownloadTemplateService {
     CSVReader reader = new CSVReader(new InputStreamReader(cs.getInputStream()));
     List<String[]> lines = reader.readAll();
     reader.close();
+
+    // Sort the maps by keys
+    Map<String, String> sortedLineCustomAttributes = new TreeMap<>(lineCustomAttributes);
+    Map<String, String> sortedCustomAttributes = new TreeMap<>(customAttributes);
+
     // Add the new column header
     List<String> headers = new ArrayList<>(Arrays.asList(lines.get(0)));
     List<String> values = new ArrayList<>();
 
     int orgIdIndex = headers.indexOf(ORG_ID);
-    lineCustomAttributes.forEach(
+
+    sortedLineCustomAttributes.forEach(
         (key, value) -> {
           headers.add(DynamicCsvHeadersValidation.LINES_CUSTOM_ATTRIBUTES_HEADER_PREFIX + key);
           values.add(value);
         });
-    customAttributes.forEach(
+    sortedCustomAttributes.forEach(
         (key, value) -> {
           headers.add(DynamicCsvHeadersValidation.CUSTOM_ATTRIBUTES_HEADER_PREFIX + key);
           values.add(value);
