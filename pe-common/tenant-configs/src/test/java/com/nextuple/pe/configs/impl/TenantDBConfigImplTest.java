006cae77639d5fd3dc9642b5a6897013f0235790
jeewan <jeewan.khatiwara@nextuple.com>
2025-06-12 19:34:10 +0530

    resolved failing tests

--- pe-common/tenant-configs/src/test/java/com/nextuple/pe/configs/impl/TenantDBConfigImplTest.java
+++ pe-common/tenant-configs/src/test/java/com/nextuple/pe/configs/impl/TenantDBConfigImplTest.java
@@ -913,20 +913,6 @@ class TenantDBConfigImplTest {
   }
 
   @Test
-  @DisplayName("Get Inventory Missing Lines Action - Exception Scenario")
-  void getInventoryMissingLinesActionExceptionTest() {
-    when(tenantConfigdataNearCacheService.get(any())).thenReturn(null);
-    PromisingEngineException ex =
-        assertThrows(
-            PromisingEngineException.class,
-            () -> tenantDBConfigImpl.getInventoryMissingLinesAction());
-    assertNotNull(ex);
-    assertEquals(
-        "Tenant Configuration not found for given orgId and configKey inventory-missing-lines-action",
-        ex.getMessage());
-  }
-
-  @Test
   @DisplayName("Get Order Operations - Happy Path")
   void getOrderOperationsTest() {
     var cacheValue = testUtil.getTenantConfigCacheValueForOrderOperations();
