ed75fe860bee1067175c26044f43f59626d2ec78
Vaibhav Singh <vaibhav.singh@nextuple.com>
2025-06-25 20:20:59 +0530

    Spotless

--- pe-item-uservice/item-consumer/src/test/java/com/nextuple/item/substitution/consumer/consumer/ItemSubstitutionBatchServiceImplTest.java
+++ pe-item-uservice/item-consumer/src/test/java/com/nextuple/item/substitution/consumer/consumer/ItemSubstitutionBatchServiceImplTest.java
@@ -7,6 +7,13 @@
 
 package com.nextuple.item.substitution.consumer.consumer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.nextuple.common.exception.CommonServiceException;
 import com.nextuple.common.response.BaseResponse;
 import com.nextuple.item.domain.feign.ItemSubstitutionFeign;
@@ -17,6 +24,8 @@ import com.nextuple.item.substitution.consumer.dto.ItemSubstitutionFeedDto;
 import com.nextuple.item.substitution.consumer.impl.ItemSubstitutionBatchServiceImpl;
 import com.nextuple.master.data.integration.enums.TaskInformation;
 import com.nextuple.master.data.integration.inbound.BatchRequest;
+import java.util.Date;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -24,132 +33,127 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Date;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 class ItemSubstitutionBatchServiceImplTest {
 
-    @InjectMocks
-    private ItemSubstitutionBatchServiceImpl itemSubstitutionBatchService;
+  @InjectMocks private ItemSubstitutionBatchServiceImpl itemSubstitutionBatchService;
 
-    @Mock
-    private ItemSubstitutionFeign itemSubstitutionFeign;
+  @Mock private ItemSubstitutionFeign itemSubstitutionFeign;
 
-    @Mock
-    private ItemSubstitutionPersistenceService itemSubstitutionPersistenceService;
+  @Mock private ItemSubstitutionPersistenceService itemSubstitutionPersistenceService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
 
-    @Test
-    void testGetTaskInformation() {
-        TaskInformation taskInformation = itemSubstitutionBatchService.getTaskInformation();
-        assertEquals(TaskInformation.ITEM_SUBSTITUTION_FEED, taskInformation);
-    }
+  @Test
+  void testGetTaskInformation() {
+    TaskInformation taskInformation = itemSubstitutionBatchService.getTaskInformation();
+    assertEquals(TaskInformation.ITEM_SUBSTITUTION_FEED, taskInformation);
+  }
 
-    @Test
-    void testCreateRecordImpl() {
-        ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
-        BaseResponse<ItemSubstitutionResponse> mockResponse = new BaseResponse<>();
-        mockResponse.setMessage("Success");
-        ResponseEntity<BaseResponse<ItemSubstitutionResponse>> mockResponseEntity = ResponseEntity.ok(mockResponse);
+  @Test
+  void testCreateRecordImpl() {
+    ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
+    BaseResponse<ItemSubstitutionResponse> mockResponse = new BaseResponse<>();
+    mockResponse.setMessage("Success");
+    ResponseEntity<BaseResponse<ItemSubstitutionResponse>> mockResponseEntity =
+        ResponseEntity.ok(mockResponse);
 
-        when(itemSubstitutionFeign.upsertItemSubstitution(any())).thenReturn(mockResponseEntity);
+    when(itemSubstitutionFeign.upsertItemSubstitution(any())).thenReturn(mockResponseEntity);
 
-        String result = itemSubstitutionBatchService.createRecordImpl(payload);
+    String result = itemSubstitutionBatchService.createRecordImpl(payload);
 
-        assertEquals("Success", result);
-        verify(itemSubstitutionFeign, times(1)).upsertItemSubstitution(any());
-    }
+    assertEquals("Success", result);
+    verify(itemSubstitutionFeign, times(1)).upsertItemSubstitution(any());
+  }
 
-    @Test
-    void testUpdateRecordImpl() throws CommonServiceException {
-        ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
-        BaseResponse<ItemSubstitutionResponse> mockResponse = new BaseResponse<>();
-        mockResponse.setMessage("Updated");
-        ResponseEntity<BaseResponse<ItemSubstitutionResponse>> mockResponseEntity = ResponseEntity.ok(mockResponse);
-        when(itemSubstitutionFeign.upsertItemSubstitution(any())).thenReturn(mockResponseEntity);
+  @Test
+  void testUpdateRecordImpl() throws CommonServiceException {
+    ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
+    BaseResponse<ItemSubstitutionResponse> mockResponse = new BaseResponse<>();
+    mockResponse.setMessage("Updated");
+    ResponseEntity<BaseResponse<ItemSubstitutionResponse>> mockResponseEntity =
+        ResponseEntity.ok(mockResponse);
+    when(itemSubstitutionFeign.upsertItemSubstitution(any())).thenReturn(mockResponseEntity);
 
-        String result = itemSubstitutionBatchService.updateRecordImpl(payload);
+    String result = itemSubstitutionBatchService.updateRecordImpl(payload);
 
-        assertEquals("Updated", result);
-        verify(itemSubstitutionFeign, times(1)).upsertItemSubstitution(any());
-    }
+    assertEquals("Updated", result);
+    verify(itemSubstitutionFeign, times(1)).upsertItemSubstitution(any());
+  }
 
-    @Test
-    void testDeleteRecordImpl() throws CommonServiceException {
-        ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
-        BaseResponse<Void> mockResponse = new BaseResponse<>();
-        mockResponse.setMessage("Deleted");
-        ResponseEntity<BaseResponse<Void>> mockResponseEntity = ResponseEntity.ok(mockResponse);
-        when(itemSubstitutionFeign.deleteItemSubstitution(any())).thenReturn(mockResponseEntity);
+  @Test
+  void testDeleteRecordImpl() throws CommonServiceException {
+    ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
+    BaseResponse<Void> mockResponse = new BaseResponse<>();
+    mockResponse.setMessage("Deleted");
+    ResponseEntity<BaseResponse<Void>> mockResponseEntity = ResponseEntity.ok(mockResponse);
+    when(itemSubstitutionFeign.deleteItemSubstitution(any())).thenReturn(mockResponseEntity);
 
-        String result = itemSubstitutionBatchService.deleteRecordImpl(payload);
+    String result = itemSubstitutionBatchService.deleteRecordImpl(payload);
 
-        assertEquals("Deleted", result);
-        verify(itemSubstitutionFeign, times(1)).deleteItemSubstitution(any());
-    }
+    assertEquals("Deleted", result);
+    verify(itemSubstitutionFeign, times(1)).deleteItemSubstitution(any());
+  }
 
-    @Test
-    void testCheckForOutdatedRecord() throws CommonServiceException {
-        BatchRequest<ItemSubstitutionFeedDto> batchRequest = new BatchRequest<>();
-        ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
-        payload.setPrimaryItemId("item1");
-        payload.setOrgId("org1");
-        payload.setPrimaryUom("EA");
-        payload.setAlternateItemId("item2");
-        payload.setAlternateUom("KG");
-        batchRequest.setPayload(payload);
-        batchRequest.setReceivedTimestamp(new Date());
+  @Test
+  void testCheckForOutdatedRecord() throws CommonServiceException {
+    BatchRequest<ItemSubstitutionFeedDto> batchRequest = new BatchRequest<>();
+    ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
+    payload.setPrimaryItemId("item1");
+    payload.setOrgId("org1");
+    payload.setPrimaryUom("EA");
+    payload.setAlternateItemId("item2");
+    payload.setAlternateUom("KG");
+    batchRequest.setPayload(payload);
+    batchRequest.setReceivedTimestamp(new Date());
 
-        ItemSubstitutionDomainDto domainDto = new ItemSubstitutionDomainDto();
-        domainDto.setLastModifiedDate(new Date(System.currentTimeMillis() - 1000));
-        Optional<ItemSubstitutionDomainDto> optionalDomainDto = Optional.of(domainDto);
+    ItemSubstitutionDomainDto domainDto = new ItemSubstitutionDomainDto();
+    domainDto.setLastModifiedDate(new Date(System.currentTimeMillis() - 1000));
+    Optional<ItemSubstitutionDomainDto> optionalDomainDto = Optional.of(domainDto);
 
-        when(itemSubstitutionPersistenceService.findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
-                "org1", "item1", "EA", "item2", "KG")).thenReturn(optionalDomainDto);
+    when(itemSubstitutionPersistenceService
+            .findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
+                "org1", "item1", "EA", "item2", "KG"))
+        .thenReturn(optionalDomainDto);
 
-        itemSubstitutionBatchService.checkForOutdatedRecord(batchRequest);
+    itemSubstitutionBatchService.checkForOutdatedRecord(batchRequest);
 
-        verify(itemSubstitutionPersistenceService, times(1))
-                .findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
-                        "org1", "item1", "EA", "item2", "KG");
-    }
+    verify(itemSubstitutionPersistenceService, times(1))
+        .findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
+            "org1", "item1", "EA", "item2", "KG");
+  }
 
-    @Test
-    void testCheckForOutdatedRecordThrowsException() {
-        BatchRequest<ItemSubstitutionFeedDto> batchRequest = new BatchRequest<>();
-        ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
-        payload.setPrimaryItemId("item1");
-        payload.setOrgId("org1");
-        payload.setPrimaryUom("EA");
-        payload.setAlternateItemId("item2");
-        payload.setAlternateUom("KG");
-        batchRequest.setPayload(payload);
-        batchRequest.setReceivedTimestamp(new Date());
+  @Test
+  void testCheckForOutdatedRecordThrowsException() {
+    BatchRequest<ItemSubstitutionFeedDto> batchRequest = new BatchRequest<>();
+    ItemSubstitutionFeedDto payload = new ItemSubstitutionFeedDto();
+    payload.setPrimaryItemId("item1");
+    payload.setOrgId("org1");
+    payload.setPrimaryUom("EA");
+    payload.setAlternateItemId("item2");
+    payload.setAlternateUom("KG");
+    batchRequest.setPayload(payload);
+    batchRequest.setReceivedTimestamp(new Date());
 
-        ItemSubstitutionDomainDto domainDto = new ItemSubstitutionDomainDto();
-        domainDto.setLastModifiedDate(new Date(System.currentTimeMillis() + 1000));
-        Optional<ItemSubstitutionDomainDto> optionalDomainDto = Optional.of(domainDto);
+    ItemSubstitutionDomainDto domainDto = new ItemSubstitutionDomainDto();
+    domainDto.setLastModifiedDate(new Date(System.currentTimeMillis() + 1000));
+    Optional<ItemSubstitutionDomainDto> optionalDomainDto = Optional.of(domainDto);
 
-        when(itemSubstitutionPersistenceService.findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
-                "org1", "item1", "EA", "item2", "KG")).thenReturn(optionalDomainDto);
+    when(itemSubstitutionPersistenceService
+            .findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
+                "org1", "item1", "EA", "item2", "KG"))
+        .thenReturn(optionalDomainDto);
 
-        CommonServiceException exception = assertThrows(CommonServiceException.class, () ->
-                itemSubstitutionBatchService.checkForOutdatedRecord(batchRequest));
+    CommonServiceException exception =
+        assertThrows(
+            CommonServiceException.class,
+            () -> itemSubstitutionBatchService.checkForOutdatedRecord(batchRequest));
 
-        assertEquals("Can't process the record as it's outdated", exception.getMessage());
-        verify(itemSubstitutionPersistenceService, times(1))
-                .findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
-                        "org1", "item1", "EA", "item2", "KG");
-    }
+    assertEquals("Can't process the record as it's outdated", exception.getMessage());
+    verify(itemSubstitutionPersistenceService, times(1))
+        .findByOrgIdAndPrimaryItemIdAndPrimaryUomAndAlternateItemIdAndAlternateUom(
+            "org1", "item1", "EA", "item2", "KG");
+  }
 }
